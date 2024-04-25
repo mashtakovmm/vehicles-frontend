ee80c2154e6c25ca932ad7d78c32168dd2133322
Max <48150531+mashtakovmm@users.noreply.github.com>
2024-04-26 02:21:06 +0300

    add git link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import "./Navbar.css"
 import Button from "../Button/Button";
 import { MdOutlineMap, MdDirectionsCar } from "react-icons/md";
+import { VscGithubInverted } from "react-icons/vsc";
+
 import { NavLink } from "react-router-dom";
 
 
@@ -12,6 +14,7 @@ const Navbar = () => {
             <nav className="nav">
                 <NavLink to='/' className="navlink"><Button icon={MdDirectionsCar} text="Cars" className="nav-button" /></NavLink>
                 <NavLink to='/map' className="navlink"> <Button icon={MdOutlineMap} text="Map" className="nav-button" /></NavLink>
+                <a className="navlink" href="https://github.com/mashtakovmm/vehicles-frontend" target="_blank"> <Button icon={VscGithubInverted} text="github" className="nav-button" /></a>
             </nav>
         </div>
     )
